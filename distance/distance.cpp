0421b8d79a326e63b7e487eb6b5f718fed6534c3
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-09-27 17:42:21 +0200

    DummyDistanceTestFromLodzToWarsaw

---
+++ distance/distance.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file cppVersion.cpp
+ *
+ * @author Jaroslaw Wiosna
+ *
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <cmath>
+
+TEST(Distance, dummyDistanceTestfromLodzToWarsaw)
+{
+    // A - Lodz
+    // B - Warsaw
+    double Alat = 51.75; //51deg 46' 36''
+    double Alon = 19.49; //19deg 27' 17''
+    double Blat = 51.24; // 51deg 13' 56''
+    double Blon = 21.01; //21deg 00' 30''
+
+    constexpr double R = 6371; // 6371000?
+    Alat = Alat * 3.14 / 180.0; // to_radians
+    Alon = Alon * 3.14 / 180.0; // to_radians
+    Blat = Blat * 3.14 / 180.0; // to_radians
+    Blon = Blon * 3.14 / 180.0; // to_radians
+    double deltaLat = Blat - Alat;
+    double deltaLon = Blon - Alon;
+
+    double a = pow(sin(0.5 * deltaLat),2) + cos(Alat) * cos(Blat) * pow(sin(0.5 * deltaLon),2);
+    double c = 2 * atan2(sqrt(a),sqrt(1-a));
+    double d = R * c;
+
+    ASSERT_TRUE( 0.8 > fabs(d - 119.0));
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
