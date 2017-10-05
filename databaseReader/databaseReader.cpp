efff6756419cff3d491b7b0e2430c3f9320f1421
Jaroslaw Wiosna <179892@edu.p.lodz.pl>
2017-10-05 19:00:36 +0200

    New tests for the database

---
+++ databaseReader/databaseReader.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file databaseReader.cpp
+ *
+ * @author Jaroslaw Wiosna
+ * @author Wojciech Mielczarek
+ * @par Licence
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <cmath>
+#include "DatabaseReader.hpp"
+
+TEST(DatabaseReader, dummyTest)
+{
+    std::string path = "../../../database/Poland-Cities010-Top10-mock.txt";
+    auto vec = DatabaseReader::readCitiesFromFile(path);
+
+    ASSERT_EQ(10, vec.size());
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
